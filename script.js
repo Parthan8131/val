95a223e6dc8f43fbd275e97c2b7163531aaaf408
Parthan8131 <114400321+Parthan8131@users.noreply.github.com>
2025-02-14 11:40:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('surprise-btn').addEventListener('click', function() {
+  document.getElementById('surprise-message').innerText = "You are my perfect match, just like a knight and rook on the chessboard!";
+});
